52643591e01b48139a7b2a0dad62e51ae0308429
Rogelio J. Baucells <rogelio@exchangelodge.com>
2025-10-03 16:11:46 -0400

    unit tests

--- src/app/workers/estimates/estimate-a.spec.ts
+++ src/app/workers/estimates/estimate-a.spec.ts
@@ -5,6 +5,43 @@ import { File } from '../../files';
 
 describe('estimates / estimateA', () => {
 
+    it('it should estimate "a" from EL500 files', (done) => {
+        // files
+        const fileNames = ['EL500_200.utd', 'EL500_250.utd', 'EL500_300.utd', 'EL500_triode.utd'];
+        // fetch all files
+        Promise.all(fileNames.map((fileName) => fetch('/test-assets/' + fileName)))
+            .then((responses) => Promise.all(responses.map((response) => response.text())))
+            .then((data) => {
+                // files
+                const files: File[] = [];
+                // loop file data
+                for (let i = 0; i < data.length; i++) {
+                    // parse file content
+                    const file = fileParserService(fileNames[i], data[i]);
+                    if (file) {
+                        // update egOffset
+                        file.egOffset = -25;
+                        // add file
+                        files.push(file);
+                    }
+                }
+                // initial
+                const initial: Initial = { mu: 4.82, ex: 1.58, kg1: 863.17, kp: 76.78, kvb: 10323.40 };
+                // estimate a
+                estimateA(initial, files, 25);
+                // check result
+                expect(initial.a).toBeCloseTo(0.0011, 4);
+                // done
+                done();
+            })
+            .catch((error) => {
+                // fail
+                fail('Failed to fetch test data: ' + error);
+                // done
+                done();
+            });
+    });
+
     it('it should estimate "a" from EF80 files', (done) => {
         // files
         const fileNames = ['EF80_200.utd', 'EF80_250.utd', 'EF80_300.utd', 'EF80_triode.utd'];
