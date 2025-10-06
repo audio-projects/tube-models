2140e93f15b16ff8a4c0cbf20a5d4b993724ba8e
Rogelio J. Baucells <rogelio@exchangelodge.com>
2025-10-06 16:25:13 -0400

    Derk Pentode model component

---
+++ src/app/components/derk-pentode-model-parameters.component.ts
@@ -0,0 +1,57 @@
+import { CommonModule } from '@angular/common';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    Output
+} from '@angular/core';
+import { ToastService } from '../services/toast.service';
+import { TubeInformation } from './tube-information';
+
+@Component({
+    selector: 'app-derk-pentode-model-parameters',
+    templateUrl: './derk-pentode-model-parameters.component.html',
+    styleUrl: './derk-pentode-model-parameters.component.scss',
+    imports: [CommonModule],
+})
+export class DerkPentodeModelParametersComponent {
+
+    @Input() tube: TubeInformation | undefined = undefined;
+    @Input() isCalculating = false;
+    @Input() canCalculate = true;
+    @Output() calculateRequested = new EventEmitter<void>();
+
+    constructor(private toastService: ToastService) {}
+
+    // Computed properties for SPICE model template
+    get spiceSubcktLine(): string {
+        const cleanTubeName = (this.tube?.name || 'PENTODE').toUpperCase().replace(/[^A-Z0-9]/g, '_');
+        return `.SUBCKT ${cleanTubeName} P G2 G1 K`;
+    }
+
+    get spiceCommentLine(): string {
+        return `* ${this.tube?.name || 'PENTODE'} Pentode Model (Derk Reefman)`;
+    }
+
+    get spiceParamLine(): string {
+        if (!this.tube?.derkModelParameters)
+            return '';
+        const params = this.tube.derkModelParameters;
+        const mu = params.mu?.toFixed(3) || '0';
+        const ex = params.ex?.toFixed(3) || '0';
+        const kg1 = params.kg1?.toFixed(6) || '0';
+        const kp = params.kp?.toFixed(6) || '0';
+        const kvb = params.kvb?.toFixed(6) || '0';
+        const kg2 = params.kg2?.toFixed(6) || '0';
+        const ccg = this.tube.ccg1 || 0;
+        const cgp = this.tube.cg1p || 0;
+        const ccp = this.tube.ccp || 0;
+        const rgi = 2000;
+        return `MU=${mu} EX=${ex} KG1=${kg1} KP=${kp} KVB=${kvb} KG2=${kg2} CCG=${ccg} CGP=${cgp} CCP=${ccp} RGI=${rgi}`;
+    }
+
+    // Trigger calculation request to parent component
+    calculateSpiceModelParameters() {
+        this.calculateRequested.emit();
+    }
+}
